e108a3244fb595c85e696d3a5387bdf00154e894
espinoza9 <ge7384005@gmail.com>
2023-02-06 17:01:28 -0400

    implementacao dos controllers

--- src/main/java/br/com/digix/pokedigix/controller/TipoController.java
+++ src/main/java/br/com/digix/pokedigix/controller/TipoController.java
@@ -4,8 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +26,27 @@ public class TipoController {
     @Autowired
     private TipoRepository tipoRepository;
 
-    @GetMapping(produces = "application/json")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Tipo>> bucarTodos() {
         Iterable<Tipo> iterable = tipoRepository.findAll();
         List<Tipo> tipos = new ArrayList<>();
         iterable.forEach(tipos::add);
         return ResponseEntity.ok().body(tipos);
     }
-    
+
+    @DeleteMapping(path = "/{id}")
+    public void remover(@PathVariable Long id) {
+        tipoRepository.deleteById(id);
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Tipo> cadastrar(@RequestBody Tipo tipo) {
+        Tipo tipoCadastrado = tipoRepository.save(tipo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tipoCadastrado);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Tipo> alterar(@RequestBody Tipo tipo) {
+        Tipo tipoAlterado = tipoRepository.save(tipo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tipoAlterado);
+}
 }
---
+++ src/test/java/br/com/digix/pokedigix/controllers/TipoControllerTest.java
@@ -0,0 +1,109 @@
+
+package br.com.digix.pokedigix.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.containsString;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import br.com.digix.pokedigix.builders.TipoBuilder;
+import br.com.digix.pokedigix.models.Tipo;
+import br.com.digix.pokedigix.repository.TipoRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TipoControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private TipoRepository tipoRepository;
+
+	@BeforeEach
+	public void deleteDdos() {
+		tipoRepository.deleteAll();
+
+	}
+
+	@Test
+	public void deve_buscar_os_tipos_cadastrados() throws Exception {
+		Tipo water = new TipoBuilder().construir();
+		Tipo fire = new TipoBuilder().comNome("Fire").construir();
+		tipoRepository.saveAll(Arrays.asList(fire, water));
+
+		this.mockMvc
+				.perform(get("/tipos"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Fire")));
+	}
+
+	@Test
+	public void deve_remover_um_tipo_pelo_id() throws Exception {
+		Tipo water = new TipoBuilder().construir();
+		Tipo fire = new TipoBuilder().comNome("Fire").construir();
+		tipoRepository.saveAll(Arrays.asList(fire, water));
+
+		this.mockMvc
+				.perform(delete("/tipos/" + water.getId()))
+				.andExpect(status().isOk());
+
+		List<Tipo> tiposRestornados = tipoRepository.findByNomeContainingIgnoreCase(water.getNome());
+
+		Assertions.assertThat(tiposRestornados).isEmpty();
+	}
+
+	@Test
+	public void deve_incluir_um_tipo() throws Exception {
+		Tipo tipo = new TipoBuilder().construir();
+		String json = toJson(tipo);
+
+		this.mockMvc
+				.perform(post("/tipos").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isCreated());
+
+		List<Tipo> tiposRetornados = tipoRepository.findByNomeContainingIgnoreCase(tipo.getNome());
+		Assertions.assertThat(tiposRetornados.size()).isEqualTo(1);
+		Assertions.assertThat(tiposRetornados.stream().map(Tipo::getNome).toList()).contains(tipo.getNome());
+
+	}
+
+	private String toJson(Tipo tipo) throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(tipo);
+		return json;
+	};
+
+	@Test
+	public void deve_poder_alterar_um_tipo() throws Exception {
+		Tipo tipoAlterado = new TipoBuilder().construir();
+		tipoRepository.save(tipoAlterado);
+		tipoAlterado.setNome("Ghost");
+
+		String json = toJson(tipoAlterado);
+
+		this.mockMvc
+				.perform(put("/tipos").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isCreated());
+		Tipo tipoRetornado = tipoRepository.findById(tipoAlterado.getId()).get();
+		Assertions.assertThat(tipoRetornado.getId()).isEqualTo(tipoAlterado.getId());
+	}
+}
